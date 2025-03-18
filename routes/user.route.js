864321374e9b8164c2de7d87d5c9ad363a9b9b21
sajidmct21 <sajidmct21@gmail.com>
2025-03-18 15:23:35 +0500

    Update user roter

--- routes/user.route.js
+++ routes/user.route.js
@@ -13,8 +13,8 @@ const router = express.Router();
 router
   .route("/getAllUsers")
   .get(getAllUsers);
-router.route("/getUserById/:id").get(verifyToken,authorizeUser('Admin','User'), getUserById);
-router.route("/updateUser/:id").patch(verifyToken, authorizeUser("Admin","User"), updateUser);
-router.route("/deleteUser/:id").delete(verifyToken,authorizeUser('Admin','User'), deleteUser);
+router.route("/getUserById/:id").get(verifyToken,authorizeUser('Admin','Manager','User'), getUserById);
+router.route("/updateUser/:id").patch(verifyToken, authorizeUser("Admin",'Manager',"User"), updateUser);
+router.route("/deleteUser/:id").delete(verifyToken,authorizeUser('Admin','Manager','User'), deleteUser);
 
 export default router;
